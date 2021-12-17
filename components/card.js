3479709acc62ac50e8cd92360ff75168b9f0d733
Megha-yg <skymegha03@gmail.com>
2021-12-17 23:08:27 +0530

    commit

---
+++ components/card.js
@@ -0,0 +1,297 @@
+import React from 'react'
+import styles from '../styles/card.module.css'
+import Image from 'next/image'
+import {useState} from "react"
+
+function Card() {
+    const [modal1,setModal1] = useState(false)
+    const [modal2,setModal2] = useState(false)
+    const [modal3,setModal3] = useState(false)
+    const [modal4,setModal4] = useState(false)
+
+
+
+    const toggleModal1 = () => {
+        setModal1(!modal1)
+    };
+    const toggleModal2 = () => {
+        setModal2(!modal2)
+    };
+    const toggleModal3 = () => {
+        setModal3(!modal3)
+    };
+    const toggleModal4 = () => {
+        setModal4(!modal4)
+    };
+
+    
+    return (
+
+        <div >
+        <div  className={styles.content}>
+            <h1 id="portfolio" className={styles.head}>PORTFOLIO</h1>
+        <div className={styles.container}>
+        <div className={styles.Card1}>
+            <div className={styles.upperContainer}>
+                
+                    <Image src="/cv.jpg " alt="" height="180px" width="300px"/>
+               
+                <div className={styles.lowerContainer}>
+                    <h3>AIR CANVAS</h3>
+                    <p> A computer vision project.</p>
+                    <button onClick={toggleModal1} className={styles.btn}>View more</button>
+                </div>
+            </div>
+         </div>  
+         {modal1 && (
+              <div className={styles.modal}>
+              <div className={styles.overlay}>
+                  <div className={styles.modalContent}>
+                      <h2>AIR CANVAS</h2>
+                      <p> It can draw anything by capturing
+                           the motion of the finger tip
+                     </p>
+                     <button className={styles.closeModal} onClick={toggleModal1} >Close</button>
+                  </div>
+              </div>
+          </div>
+         )} 
+         <div className={styles.Card1}>
+            <div className={styles.upperContainer}>
+                
+                    <Image src="/js.jpg " alt="" height="180px" width="300px"/>
+               
+                <div className={styles.lowerContainer}>
+                    <h3>CODE PLAYER</h3>
+                    <p>Code debugger</p>
+                    <button onClick={toggleModal2} className={styles.btn}>View more</button>
+                </div>
+            </div>
+         </div>  
+         {modal2 && (
+              <div className={styles.modal}>
+              <div className={styles.overlay}>
+                  <div className={styles.modalContent}>
+                      <h2>CODE PLAYER</h2>
+                      <p>  It helps to test snippets of  javascript and css code, 
+               within some context, and debug the code collaboratively.
+                     </p>
+                     <button className={styles.closeModal} onClick={toggleModal2} >Close</button>
+                  </div>
+              </div>
+          </div>
+         )} 
+         </div>
+         <div className={styles.container}>
+         <div className={styles.Card1}>
+            <div className={styles.upperContainer}>
+                
+                    <Image src="/ecg.jpg " alt="" height="180px" width="300px"/>
+               
+                <div className={styles.lowerContainer}>
+                    <h3>HEALTH MONITORING</h3>
+                    <p>Arduino based health monitoring system</p>
+                    <button onClick={toggleModal3} className={styles.btn}>View more</button>
+                </div>
+            </div>
+         </div>  
+         {modal3 && (
+              <div className={styles.modal}>
+              <div className={styles.overlay}>
+                  <div className={styles.modalContent}>
+                      <h2>HEALTH MONITORING</h2>
+                      <p> Arduino based health monitoring system.
+                     </p>
+                     <button className={styles.closeModal} onClick={toggleModal3} >Close</button>
+                  </div>
+              </div>
+          </div>
+         )} 
+         <div className={styles.Card1}>
+            <div className={styles.upperContainer}>
+                
+                    <Image src="/car.jpg " alt="" height="180px" width="300px"/>
+               
+                <div className={styles.lowerContainer}>
+                    <h3>DTMF CAR</h3>
+                    <p> Mobile controlled car.</p>
+                    <button onClick={toggleModal4} className={styles.btn}>View more</button>
+                </div>
+            </div>
+         </div>  
+         {modal4 && (
+              <div className={styles.modal}>
+              <div className={styles.overlay}>
+                  <div className={styles.modalContent}>
+                      <h2>DTMF CAR</h2>
+                      <p>  DTMF based mobile controlled robotic car.
+                     </p>
+                     <button className={styles.closeModal} onClick={toggleModal4} >Close</button>
+                  </div>
+              </div>
+          </div>
+         )} 
+        
+        </div>
+        </div>
+        <div className={styles.card1}>
+         
+            <div className={styles.card2}>
+            
+                <Image
+                    
+                    src="/cv.jpg"
+                  alt="Air canvas"
+                  height="300px"
+                  width="550px"
+                    
+                     
+                />
+            </div>
+            <div className={styles.overlay}>
+                <div className={styles.title}>
+                    <Link href="/">AIR CANVAS</Link>
+                </div>
+                <p className={styles.desc}>
+                    A computer vision project.<br/>
+                    It can draw anything by capturing the motion of the finger tip
+                </p>
+
+            </div>
+
+            </div>
+            <div className={styles.card1}>
+         
+            <div className={styles.card2}>
+            
+                    <Image
+                    
+                    src="/js.jpg"
+                    alt="Code Player"
+                    height="300px"
+                    width="550px"
+                   
+                     
+                />
+            </div>
+            <div className={styles.overlay}>
+                <div className={styles.title}>
+                <Link href="/">CODE PLAYER</Link>
+                </div>
+                <p className={styles.desc}>
+               It helps to test snippets of  javascript and css code, 
+               within some context, and debug the code collaboratively.
+               </p>
+
+            </div>
+
+            </div>
+
+            <div className={styles.card1}>
+         
+            <div className={styles.card2}>
+            
+                    <Image
+                    
+                    src="/ecg.jpg"
+                    alt="Health monitoring"
+                    height="300px"
+                    width="550px"
+                    
+                     
+                />
+            </div>
+            <div className={styles.overlay}>
+                <div className={styles.title}>
+                    <Link href="/">HEALTH MONITORING SYSTEM</Link>
+            
+                </div>
+                <p className={styles.desc}>
+                    Arduino based health monitoring system.
+                </p>
+
+            </div>
+
+            </div>
+
+            <div className={styles.card1}>
+         
+                <div className={'styles.card2'}>
+            
+                    <Image
+                   
+                    src="/car.jpg"
+                    alt="Car"
+                   
+                   height="300px"
+                   width="550px"
+                   
+                    
+                     
+                    />
+                </div>
+                <div className={styles.overlay}>
+                    <div className={styles.title}>
+                        <Link href="/">Mobile Controlled CAR</Link>
+                    </div>
+                    <p className={styles.desc}>
+                        DTMF based mobile controlled robotic car.
+                    </p>
+
+                </div>
+
+                </div>
+
+                <div className={styles.card3}>
+            
+                <Image
+                    
+                    src="/cv.jpg"
+                  alt="Air canvas"
+                  height="150px"
+                  width="250px"
+                    
+                     
+                />
+            </div>
+            <div className={styles.card3}>
+            
+                <Image
+                    
+                    src="/js.jpg"
+                  alt="Air canvas"
+                  height="150px"
+                  width="250px"
+                    
+                     
+                />
+            </div>
+            <div className={styles.card3}>
+            
+                <Image
+                    
+                    src="/ecg.jpg"
+                  alt="health"
+                  height="150x"
+                  width="250px"
+                    
+                     
+                />
+            </div>
+            <div className={styles.card3}>
+            
+                <Image
+                    
+                    src="/car.jpg"
+                  alt="robot"
+                  height="150px"
+                  width="250px"
+                    
+                     
+                />
+            </div>
+        </div>
+    )
+}
+
+export default Card
