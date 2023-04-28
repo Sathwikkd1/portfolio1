17624b22927fd579afd5e617576edf16e84ed9c2
Megha Y Gowda <93361833+Megha-yg@users.noreply.github.com>
2023-04-28 15:03:04 +0530

    Update Education.js

--- components/Education.js
+++ components/Education.js
@@ -1,18 +1,14 @@
 import styles from "../styles/Education.module.css";
 import Image from "next/image";
 import Link from "next/link";
-import { Button } from "reactstrap";
+
 
 function Education() {
   return (
       
     
       <div className={styles.container} id="education">
-            <h1 className={styles.head}>SignUp</h1>
-             <Button color="primary" type="button">
-                <Link href="/books" passref>Signup</Link>
-             </Button>
-
+           
             
             
             
